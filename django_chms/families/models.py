6f90cb8e3eb9beb32df4691f9caada80fa0c47f5
James Doepp <jim@jimlaptop.LinuxGeek>
2017-04-20 22:39:53 -0400

    Add user to Family table

--- django_chms/families/models.py
+++ django_chms/families/models.py
@@ -2,12 +2,14 @@ from datetime import date
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Family(models.Model):
     class Meta:
         verbose_name_plural = 'Families'
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     family_name = models.CharField(max_length=50)
     address1 = models.CharField(blank=True, max_length=255)
     address2 = models.CharField(blank=True, max_length=255)
@@ -30,7 +32,7 @@ class Member(models.Model):
     title = models.CharField(blank=True, max_length=15)
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(blank=True, max_length=255)
-    last_name = models.CharField(default=Family.family_name, max_length=50)
+    last_name = models.CharField(max_length=50)
     suffix = models.CharField(blank=True, max_length=15)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
     birth_date = models.DateField(blank=True, null=True)
@@ -38,6 +40,10 @@ class Member(models.Model):
     notes = models.TextField(blank=True)
     def __str__(self):
         return '%s, %s' % (self.last_name, self.first_name)
+
+    def save(self, *args, **kwargs):
+        self.last_name = self.family.family_name
+        super(Member, self).save(*args, **kwargs)
 
 class Adult(Member):
     occupation = models.CharField(blank=True, max_length=255)
