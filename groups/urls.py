205151f11a57df87cf46720541b239c1a1743760
pihentagyu <jdoepp@gmail.com>
2017-07-30 23:29:38 -0400

    Group edit/delete

--- groups/urls.py
+++ groups/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
         url(r'^$', views.GroupListView.as_view(), name='group_list'),
         url(r'create_group/$', views.GroupCreateView.as_view(), name='create_group'),
+        url(r'delete_group_(?P<pk>\d+)/$', views.GroupDeleteView.as_view(), name='delete_group'),
+        url(r'edit_group_(?P<pk>\d+)/$', views.GroupUpdateView.as_view(), name='edit_group'),
+        url(r'search/$', views.GroupSearchView.as_view(), name='search'), 
         url(r'(?P<pk>\d+)/$', views.GroupDetailView.as_view(), name='group_detail'),
         ]
--- groups/views.py
+++ groups/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import ListView, DetailView,CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.conf import settings
 from . import models
 
@@ -23,12 +23,42 @@ class GroupListView(PrefetchRelatedMixin, ListView):
 class GroupCreateView(LoginRequiredMixin, CreateView):
     fields = ('group_name', 'group_type', 'group_description')
     template_name = 'groups/group_form.html'
-    success_url = reverse_lazy('groups:list')
+    success_url = reverse_lazy('groups:group_list')
     model = models.Group
 
 
 class GroupDetailView(PrefetchRelatedMixin, DetailView):
-    prefetch_related = ('groups_adult_related', 'groups_child_related')
+    prefetch_related = ('groups_groupadultmember_related', 'groups_groupchildmember_related')
     model = models.Group
     template_name = 'groups/group_detail.html'
 
+
+class GroupDeleteView(DeleteView):
+    model = models.Group
+    success_url = reverse_lazy('groups:group_list')
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return self.model.objects.filter(user=self.request.user)
+        else:
+            return self.model.objects.all()
+
+
+class GroupUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Group
+    fields = ('group_name', 'group_type', 'group_description')
+
+class GroupSearchView(PrefetchRelatedMixin, ListView):
+    prefetch_related = ('groups_groupadultmember_related', 'groups_groupchildmember_related')
+    template_name = 'groups/group_list.html'
+    model = models.Group
+    context_object_name = 'groups'
+
+    def get_queryset(self):
+        term = self.request.GET.get('q')
+        return self.model.objects.filter(Q(group_name__icontains=term)|Q(group_description__icontains=term)|Q(group_type__group_type__icontains=term)).distinct()
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['email'] = 'jdoepp@gmail.com'
+        return context
