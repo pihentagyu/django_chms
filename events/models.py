c0d4cb4b3b7b90004fdf04046b84d49c0fda85a8
pihentagyu <jdoepp@gmail.com>
2017-08-19 16:13:48 -0400

    Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    EVENT_TYPES = ('simple', 'recurring',)
+
+    class Meta:
+        abstract = True
+    begin_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    event_type = 
+
+
