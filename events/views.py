ce3d46489b673118ef842e1fec5b1e90b8d22791
pihentagyu <jdoepp@gmail.com>
2017-09-12 08:15:00 -0400

    Event CRUD

--- events/views.py
+++ events/views.py
@@ -1,11 +1,32 @@
+from braces.views import PrefetchRelatedMixin
+from datetime import datetime
 from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
 from django.db.models import Prefetch, Q
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
-
+from extra_views import InlineFormSet, CreateWithInlinesView, UpdateWithInlinesView
+from extra_views.generic import GenericInlineFormSet
 from . import models
 
 # Create your views here.
+
+class EventListView(PrefetchRelatedMixin, ListView):
+    model = models.Occurrence
+    template_name = 'events/event_list.html'
+    context_object_name = 'events'
+    ordering = ['begin_time']
+    paginate_by = 10
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['church_name'] = settings.CHURCH_NAME
+        return context
+
+    def get_queryset(self):
+        return self.model.objects.filter(begin_time__date__gte=datetime.today())
+
 
 class EventYearlyListView(ListView):
     model = models.Occurrence
@@ -67,6 +88,37 @@ class EventDailyListView(ListView):
 
 class EventDetailView(DeleteView):
     model = models.Event
-    context_object_name = 'events'
-    template_name = "events/event_list.html"
+    context_object_name = 'event'
+    template_name = "events/event_detail.html"
+
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    fields = ('name', 'location', 'group', 'description', 'creator')
+    template_name = 'events/event_form.html'
+    success_url = reverse_lazy('events:event_list')
+    model = models.Event
+
+
+class OccurrenceInline(InlineFormSet):
+    fields = ('begin_time', 'end_time')
+    model = models.Occurrence
+
+
+#class EventUpdateView(LoginRequiredMixin, UpdateView):
+class EventUpdateView(LoginRequiredMixin, UpdateWithInlinesView):
+    model = models.Event
+    template_name = 'events/event_form.html'
+    fields = ('name', 'location', 'group', 'description', 'creator')
+    inlines = (OccurrenceInline,)
+
+
+class EventDeleteView(DeleteView):
+    model = models.Event
+    success_url = reverse_lazy('events:event_list')
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return self.model.objects.filter(user=self.request.user)
+        else:
+            return self.model.objects.all()
 
